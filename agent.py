1e03233064c0244dc3447535a7c6410de60b6d93
tin200 <88190836+tin200@users.noreply.github.com>
2024-11-30 14:57:20 +0100

    created create_agents function

--- agent.py
+++ agent.py
@@ -1,4 +1,6 @@
 from typing import List
+import random
+import numpy as np
 
 class Agent:
     def __init__(self, 
@@ -17,4 +19,23 @@ class Agent:
 
 
 def create_agents():
-    print(1)
+    age = random.randint(18, 65)
+    behaviour = np.random.choice(["Impulsive behavior: refers to quick, often emotional decisions without much thought or evaluation. Impulsive buyers are driven by immediate desires rather than rational planning", "Reflective behavior: refers to thoughtful, deliberate purchasing decisions based on careful evaluation. Reflective buyers take their time, compare options, and focus on long-term value"], p=[0.6, 0.4])
+    use_case = np.random.choice(["work", "travel", "personal use"])
+    tech_savvy = np.random.choice([True, False], p=[0.6, 0.4])
+    brand_conscious = np.random.choice([True, False], p=[0.6, 0.4])
+    devices = np.random.choice(['smartphone', 'PC', 'tablet', 'altri'], p=[0.5, 0.3, 0.1, 0.1])
+
+    agent = Agent(
+        age=age,
+        behavior=behaviour,
+        use_case=use_case,
+        tech_savvy=tech_savvy,
+        brand_conscious=brand_conscious,
+        devices=devices
+    )
+
+    return agent
+
+if __name__ == "__main__":
+    create_agents()
